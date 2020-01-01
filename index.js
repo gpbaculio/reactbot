e4e77e54f60492f0fb81482a52313ce91af3d769
Glendon Philipp <gpbaculio@gmail.com>
2020-01-02 00:51:55 +0800

    fix port

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
 app.get('/', (req, res) => {
   res.send({ 'hello': 'there' })
 })
 
-app.listen(5000)
+app.listen(PORT)
