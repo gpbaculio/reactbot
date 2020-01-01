de75d679329bee69f64af35bf2b5b7e316f383d4
Glendon Philipp <gpbaculio@gmail.com>
2020-01-02 04:50:09 +0800

    add route handles dirs

---
+++ chatBot/chatBot.js
@@ -0,0 +1,68 @@
+const dialogFlow = require('dialogflow')
+const structJson = require('structjson')
+
+const config = require('../config/keys')
+
+
+const projectId = config.googleProjectId
+const languageCode = config.dialogFlowSessionLanguageCode
+const credentials = {
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey
+}
+
+const sessionClient = new dialogFlow.SessionsClient({
+  projectId,
+  credentials
+});
+const sessionPath = sessionClient.sessionPath(
+  config.googleProjectId,
+  config.dialogFlowSessionId
+);
+
+
+module.exports = {
+  textQuery: async (text, parameters = {}) => {
+    let self = module.exports
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          // The query to send to the dialogflow agent
+          text,
+          // The language used by the client (en-US)
+          languageCode,
+        },
+      },
+      queryParams: {
+        payload: {
+          data: parameters
+        }
+      }
+    };
+    let responses = await sessionClient.detectIntent(request)
+    responses = await self.handleAction(responses)
+    return responses
+  },
+  eventQuery: async (event, parameters = {}) => {
+    let self = module.exports
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        event: {
+          // The query to send to the dialogflow agent
+          name: event,
+          parameters: structJson.jsonToStructProto(parameters),
+          // The language used by the client (en-US)
+          languageCode,
+        },
+      }
+    };
+    let responses = await sessionClient.detectIntent(request)
+    responses = await self.handleAction(responses)
+    return responses
+  },
+  handleAction: responses => {
+    return responses
+  }
+}
---
+++ routes/dialogFlowRoutes.js
@@ -0,0 +1,34 @@
+
+const chatBot = require('../chatBot/chatBot')
+
+module.exports = app => {
+
+  app.get('/', (req, res) => {
+    res.send({ 'hello': 'jany yes papa' })
+  })
+
+  app.post('/api/df_text_query', async (req, res) => {
+    try {
+      const responses = await chatBot.textQuery(
+        req.body.text,
+        req.body.parameters
+      );
+      res.send({ result: responses[0].queryResult })
+    } catch (error) {
+      res.send({ error })
+    }
+  })
+
+  app.post('/api/df_event_query', async (req, res) => {
+    try {
+      const responses = await chatBot.eventQuery(
+        req.body.event,
+        req.body.parameters
+      );
+      res.send({ result: responses[0].queryResult })
+    } catch (error) {
+      res.send({ error })
+    }
+  })
+
+}
